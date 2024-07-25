327f4a882a5ed9407f1e76ac293059263aba0805
Himesh Tyagi <himeshtyagi16@gmail.com>
2024-07-25 23:37:23 +0530

    Update diabetesapp.py

--- diabetesapp.py
+++ diabetesapp.py
@@ -14,14 +14,14 @@ with open('sc.pkl', 'rb') as scaler_file:
 st.title('Diabetes Prediction App')
 
 # Input fields
-Pregnancies = st.number_input('Pregnancies', min_value=0.0, max_value=20.0, value=0.0, step=1.0)
-Glucose = st.number_input('Glucose', min_value=0.0, max_value=400.0, value=30.0, step=1.0)
-BloodPressure = st.number_input('BloodPressure', min_value=0.0, max_value=200.0, value=0.0, step=1.0)
-SkinThickness = st.number_input('SkinThickness', min_value=0.0, max_value=200.0, value=0.0, step=1.0)
-Insulin = st.number_input('Insulin', min_value=0.0, max_value=700.0, value=0.0, step=1.0)
-DiabetesPedigreeFunction = st.number_input('DiabetesPedigreeFunction', min_value=0.0, max_value=2.0, value=0.0, step=0.01)
-BMI = st.number_input('BMI', min_value=0.0, max_value=150.0, value=20.0, step=0.1)
-Age = st.number_input('Age', min_value=15.0, max_value=100.0, value=25.0, step=1.0)
+Pregnancies = st.sidebar.number_input('Pregnancies', min_value=0.0, max_value=20.0, value=0.0, step=1.0)
+Glucose = st.sidebar.number_input('Glucose', min_value=0.0, max_value=400.0, value=30.0, step=1.0)
+BloodPressure = st.sidebar.number_input('BloodPressure', min_value=0.0, max_value=200.0, value=0.0, step=1.0)
+SkinThickness = st.sidebar.number_input('SkinThickness', min_value=0.0, max_value=200.0, value=0.0, step=1.0)
+Insulin = st.sidebar.number_input('Insulin', min_value=0.0, max_value=700.0, value=0.0, step=1.0)
+DiabetesPedigreeFunction = st.sidebar.number_input('DiabetesPedigreeFunction', min_value=0.0, max_value=2.0, value=0.0, step=0.01)
+BMI = st.sidebar.number_input('BMI', min_value=0.0, max_value=150.0, value=20.0, step=0.1)
+Age = st.sidebar.number_input('Age', min_value=15.0, max_value=100.0, value=25.0, step=1.0)
 
 # Prepare the feature vector
 features = np.array([[Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age]], dtype=np.float64)
